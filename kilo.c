704b83af1e5fcd4dddbbf345135cfb3258942a60
Joseph <quartz135@gmail.com>
2024-01-31 16:11:32 +1300

    refactor enableRawMode to return terminal to original settings on program exit

--- kilo.c
+++ kilo.c
@@ -12,8 +12,9 @@ void disableRawMode(){
 }
 
 void enableRawMode(){
-    struct terminos raw;
-    tcgetattr(STDIN_FILENO, &raw);
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+    struct termios raw = orig_termios;
     raw.c_lflag &= ~(ECHO);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
