ae5e84bf964afa8a93958a142132bfedce6bd8e8
Joseph <quartz135@gmail.com>
2024-01-31 16:06:25 +1300

    add function to modify terminal attributes in canonical mode to print out keys

--- kilo.c
+++ kilo.c
@@ -2,8 +2,19 @@
 // Created by joseph on 31/01/24.
 //
 #include <unistd.h>
+#include <termio.h>
+
+void enableRawMode(){
+    struct terminos raw;
+    tcgetattr(STDIN_FILENO, &raw);
+    raw.c_lflag &= ~(ECHO);
+    tcsetattr(STDIN_FILENO, TCAFLUSH, &raw);
+}
+
 
 int main(){
+    enableRawMode();
+
     char c;
     while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
