2880912440dc6775883b051e89fca0bb78a60dd6
Joseph <quartz135@gmail.com>
2024-01-31 16:30:29 +1300

    add final flags to terminal settings to enable raw mode, and include comments

--- kilo.c
+++ kilo.c
@@ -7,19 +7,27 @@
 #include <termios.h>
 #include <unistd.h>
 
+//struct copy of the original terminal settings
 struct termios orig_termios;
 
 void disableRawMode(){
+    //apply original terminal settings
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
 void enableRawMode(){
+    //get original terminal settings
     tcgetattr(STDIN_FILENO, &orig_termios);
+    //set disableRawMode function to execute on program exit
     atexit(disableRawMode);
+    //create a copy of terminal settings and apply custom flags to enable "raw" mode
     struct termios raw = orig_termios;
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_iflag &= ~(ICRN | IXON);
     raw.c_oflag &= ~(OPOST);
+    raw.c_cflag &= ~(CS8);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    //apply new terminal settings
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
