1c3ee637bc611dc50e134037850cdcb436699bab
Joseph <quartz135@gmail.com>
2024-01-31 16:26:03 +1300

    include new flag to disable automatic carriage returns from the terminal in prep for adding custom

--- kilo.c
+++ kilo.c
@@ -18,6 +18,7 @@ void enableRawMode(){
     atexit(disableRawMode);
     struct termios raw = orig_termios;
     raw.c_iflag &= ~(ICRN | IXON);
+    raw.c_oflag &= ~(OPOST);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
