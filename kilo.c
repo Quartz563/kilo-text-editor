df47b09027528c23c321db9038acbae9d0ded985
Joseph <quartz135@gmail.com>
2024-01-31 16:59:51 +1300

    add new control key edit macro and updated the error handling in the main function

--- kilo.c
+++ kilo.c
@@ -11,6 +11,10 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** defines ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 /*** data ***/
 
 //struct copy of the original terminal settings
@@ -57,14 +61,14 @@ int main(){
 
     while(1){
         char c = '\0';
-        if(read(STDIN_FILENO, &c, 1) == -1)
+        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
             die("read");
         if(iscntrl(c)){
             printf("%d\r\n", c);
         } else {
             printf("%d ('%c')\r\n", c, c);
         }
-        if(c == 'q') break;
+        if(c == CTRL_KEY('q')) break;
     }
     return 0;
 }
